269fd5f0969b293eec5c750a7e2ff83943786344
Veronica <vgalbicka@gmail.com>
2024-04-27 23:00:37 -0300

    Initial commit

---
+++ src/main/java/com/aluracursos/screenmatch2/Screenmatch2Application.java
@@ -0,0 +1,18 @@
+package com.aluracursos.screenmatch2;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Screenmatch2Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Screenmatch2Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hola Mundo Vero");
+	}
+}
