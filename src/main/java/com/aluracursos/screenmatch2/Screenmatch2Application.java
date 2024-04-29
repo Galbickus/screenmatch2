47478254be92407e5cd9b42031145c0d7003c3c7
Veronica <vgalbicka@gmail.com>
2024-04-29 02:01:41 -0300

    changes

--- src/main/java/com/aluracursos/screenmatch2/Screenmatch2Application.java
+++ src/main/java/com/aluracursos/screenmatch2/Screenmatch2Application.java
@@ -2,11 +2,15 @@ package com.aluracursos.screenmatch2;
 
 import com.aluracursos.screenmatch2.model.DatosEpisodio;
 import com.aluracursos.screenmatch2.model.DatosSerie;
+import com.aluracursos.screenmatch2.model.DatosTemporadas;
 import com.aluracursos.screenmatch2.service.ConsumoAPI;
 import com.aluracursos.screenmatch2.service.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class Screenmatch2Application implements CommandLineRunner {
@@ -27,5 +31,13 @@ public class Screenmatch2Application implements CommandLineRunner {
 		//la nomenclatura de la instancia debería ser singular
 		DatosEpisodio episodios = conversor.obtenerDatos(json, DatosEpisodio.class);
 		System.out.println(episodios);
+
+		List<DatosTemporadas> temporadas = new ArrayList<>();
+		for (int i = 1; i <=datos.totalDeTemporadas(); i++) {
+			json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=Game+of+Thrones&Season="+ i +"&apikey=26dd704c");
+			var datosTemporadas = conversor.obtenerDatos(json, DatosTemporadas.class);
+			temporadas.add(datosTemporadas);
+		}
+		temporadas.forEach(System.out::println);
 	}
 }
